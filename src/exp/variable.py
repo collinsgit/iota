cd04df396984ff4afaa4550f6990efb6e29f1250
Collin Potts <collinpotts@gmail.com>
2019-12-22 18:03:27 -0500

    Implement logarithms

--- src/exp/variable.py
+++ src/exp/variable.py
@@ -1,5 +1,4 @@
-from functools import reduce
-import operator
+import math
 from typing import Dict, Union
 
 
@@ -19,7 +18,7 @@ def simplify(f):
     def wrapper(*args, **kwargs):
         output = f(*args, **kwargs)
 
-        return output.eval() if not isinstance(output, Val.__args__) else output
+        return output.eval() if isinstance(output, Value) else output
     return wrapper
 
 
@@ -116,6 +115,7 @@ class Constant(Value):
 class Operator(Value):
     precedence = 0
 
+    @make_constants
     def __init__(self, *vals: Value):
         super().__init__()
 
@@ -125,12 +125,13 @@ class Operator(Value):
         raise NotImplementedError
 
 
-def parenthesize(val: Value, paren_type: type = Operator, precedence: int = 0):
+def parenthesize(val: Value, paren_type: type = Operator, precedence: float = 0.):
     val_str = str(val)
     val_precedence = val.precedence if isinstance(val, Operator) else 0
 
     if val_precedence < precedence and isinstance(val, paren_type):
-        val_str = '(' + val_str + ')'
+        if val_str[0] != '(' or val_str[-1] != ')':
+            val_str = '(' + val_str + ')'
     return val_str
 
 
@@ -145,8 +146,8 @@ class BinaryOperator(Operator):
     def __str__(self):
         sub_strings = []
 
-        for val in self.vals:
-            sub_string = parenthesize(val, precedence=self.precedence)
+        for i, val in enumerate(self.vals):
+            sub_string = parenthesize(val, precedence=self.precedence + (0.1 if i else 0.))
 
             sub_strings.append(sub_string)
 
@@ -267,8 +268,60 @@ class Power(Operator):
         return self.vals[1] * self.vals[0].diff(wrt) * self.vals[0] ** (self.vals[1] - 1)
 
 
+class Logarithm(Operator):
+    precedence = 2
+
+    def __str__(self):
+        if len(self.vals) == 1:
+            antilog = str(self.vals[0])
+
+            return 'ln({})'.format(antilog)
+        elif len(self.vals) == 2:
+            base = str(self.vals[0])
+            antilog = str(self.vals[1])
+
+            return 'log({}, {})'.format(base, antilog)
+        else:
+            raise NotImplementedError
+
+    def eval(self, val_dict=None):
+        if len(self.vals) == 1:
+            base = math.e
+            antilog = self.vals[0].eval(val_dict)
+        else:
+            base = self.vals[0].eval(val_dict)
+            antilog = self.vals[1].eval(val_dict)
+
+        if isinstance(base, Val.__args__):
+            if antilog == 1.:
+                return 0.
+            elif antilog == base:
+                return 1.
+
+            if isinstance(antilog, Val.__args__):
+                return math.log(antilog, base)
+            else:
+                return Logarithm(antilog) if base == math.e else Logarithm(base, antilog)
+        else:
+            raise NotImplementedError
+
+    @simplify
+    def diff(self, wrt):
+        if len(self.vals) == 1:
+            base = math.e
+            antilog = self.vals[0]
+        else:
+            base = self.vals[0].eval()
+            antilog = self.vals[1]
+
+        if isinstance(base, Val.__args__):
+            return antilog.diff(wrt) / (math.log(base) * antilog)
+        else:
+            raise NotImplementedError
+
+
 if __name__ == '__main__':
-    exp = 5 + ((Constant(1.) * Constant(3.) - Variable('x')) / Constant(4.) + 4) ** 5
+    exp = Logarithm(5 + ((Constant(1.) * Constant(3.) - Variable('x')) / Constant(4.) + 4) ** 5)
 
     print(exp)
     print(exp.eval())
