5b3bd12fa28dc002f1fead0e5e5b146f09925fa4
Madelyn Torres <madelyn.torres1198@gmail.com>
2020-03-18 13:39:27 -0700

    Quick fix in __init__.

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,5 @@
 # Import relevant modules from the directory (for external use and testing).
 if __name__ is not None and "." in __name__:
-    from .grade_sheet import Category
-    from .grade_sheet import Grade
-    from .grade_sheet import GradeSheet
-    from .grade_sheet import GradeSheetGeneric
-    from .parse_grade_sheet import ParseGradeSheet
+    from .iota import Val
 else:
-    from grade_sheet import Category
-    from grade_sheet import Grade
-    from grade_sheet import GradeSheet
-    from grade_sheet import GradeSheetGeneric
-    from parse_grade_sheet import ParseGradeSheet
+    from iota import Val
