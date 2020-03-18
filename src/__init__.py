49c4ccae17a086d299457418e55e452d11809c21
Madelyn Torres <madelyn.torres1198@gmail.com>
2020-03-18 13:51:18 -0700

    Fixed and added basic templates for tests.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,2 @@
 # Import relevant modules from the directory (for external use and testing).
-if __name__ is not None and "." in __name__:
-    from .iota import Val
-else:
-    from iota import Val
+from .iota import Val
---
+++ src/tests/test_iota.py
@@ -0,0 +1,10 @@
+import unittest
+
+from .. import Val
+
+
+class TestVal(unittest.TestCase):
+    """ TODO: Add real tests and get rid of this testing class """
+
+    def test_test(self):
+        self.assertTrue(True)
