48439077601508f8199dc4dce762f84fbb306562
Collin Potts <collinpotts@gmail.com>
2020-03-18 22:56:32 -0400

    Comment variable.py

--- iota/objects/variable.py
+++ iota/objects/variable.py
@@ -9,21 +9,28 @@ from .value import Val, Value
 
 
 class Constant(Value):
+    """
+    Representation of a constant (integer or floating point)
+    """
+
     def __init__(self, val: Val):
         super().__init__()
 
         self.val = val
 
     def eval(self, val_dict=None):
+        # Evaluates to self
         return self.val
 
     def __str__(self):
         return str(self.val)
 
     def diff(self, wrt):
+        # Constant always has derivative of 0
         return 0
 
     def __eq__(self, other):
+        # If both Constant, compare vals; otherwise, compare self.val to other
         if isinstance(other, Constant):
             return self.val == other.val
         elif isinstance(other, Val.__args__):
@@ -32,16 +39,28 @@ class Constant(Value):
 
 
 class Variable(Value):
+    """
+    Representation of a variable (to be evaluated)
+    """
+
     def __init__(self, name: str):
         super().__init__()
 
         self.name = name
 
     def eval(self, val_dict=None):
+        # Evaluate to value if given in val_dict, otherwise remain
         return val_dict.get(self.name, self) if val_dict is not None else self
 
     def __str__(self):
         return self.name
 
     def diff(self, wrt):
+        # Derivative is 1 if Variable is subject of differentation, else 0
         return 1 if wrt == self.name else 0
+
+    def __eq__(self, other):
+        # Compare variable names
+        if isinstance(other, Variable):
+            return self.name == other.name
+        return False
